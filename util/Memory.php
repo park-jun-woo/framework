4c2ca769dbc3e359cfab51d4f37f6f11c6b5068a
mados <mados@DESKTOP-SMHR0F2>
2023-09-23 14:27:05 +0900

    .

--- util/Memory.php
+++ util/Memory.php
@@ -1,34 +1,106 @@
 <?php
 namespace util;
 
+use Parkjunwoo;
+
 class Memory{
-	public static function get(string $key){
+	protected string $serverKey;
+	protected array $countSemaphores;
+	protected array $lockSemaphores;
+	
+	public function __construct(int $serverKey){
+		$this->serverKey = (string)$serverKey;
+		$this->countSemaphores = array();
+		$this->lockSemaphores = array();
+	}
+	
+	public function get(string $key){
 		return apcu_fetch($key);
 	}
 	
-	public static function set(string $key, $value):int{
+	public function set(string $key, $value):int{
 		
 		apcu_store($key, $value);
 	}
 	
-	public static function delete(string $key):bool{
+	public function delete(string $key):bool{
 		return apcu_delete($key);
 	}
 	
-	public static function lock(string $key, int $operation, int &$wouldblock=null):bool{
+	public function lock(string $key, int $operation, int &$wouldblock=null):bool{
+		$countKey = "{$this->serverKey}c{$key}";
+		$lockKey = "{$this->serverKey}l{$key}";
+		if(array_key_exists($countKey, $this->countSemaphores)){$countSemaphore = $this->countSemaphores[$countKey];}
+		else{$countSemaphore = $this->countSemaphores[$countKey] = sem_get($countKey);}
+		if(array_key_exists($lockKey, $this->lockSemaphores)){$lockSemaphore = $this->lockSemaphores[$lockKey];}
+		else{$lockSemaphore = $this->lockSemaphores[$lockKey] = sem_get($lockKey);}
+		
 		switch($operation){
 			case LOCK_SH:
-				$countSem = sem_get("c".$key);
-				$lockSem = sem_get("l".$key);
-				if(sem_acquire($countSem)){
-					if(apcu_exists("c".$key) && apcu_fetch("c".$key)==0){
-						
+				//카운터 락 획득
+				if(sem_acquire($countSemaphore)){
+					//메모리에 주어진 키에 대해 카운터가 존재하지 않거나 0이라면
+					if(!apcu_exists($countKey) || ($count = apcu_fetch($countKey))==0){
+						apcu_store($countKey, 1);
+						//락 획득
+						sem_acquire($lockSemaphore);
+						//카운터 락 해제
+						sem_release($countSemaphore);
+					}
+					//카운터가 -1이라면 배제락 상태이므로 카운터락을 해제하고 배제락이 풀릴 때까지 대기한다.
+					else if($count==-1){
+						$acquired = sem_acquire($lockSemaphore, true);
+						//카운터 락 해제
+						sem_release($countSemaphore);
+						//락 획득할 때까지 대기한다.
+						if(!$acquired){sem_acquire($lockSemaphore);}
+						//락을 획득하면 다시 카운터 락을 걸고 카운터에 1저장하고 카운터 락 해제한다.
+						if(sem_acquire($countSemaphore)){
+							apcu_store($countKey, 1);
+							//카운터 락 해제
+							sem_release($countSemaphore);
+						}
+					}
+					//카운터가 0도 아니고 -1도 아니라면 공유락 상태이므로 카운트를 1 올리고 그냥 진행한다.
+					else{
+						apcu_inc($countKey,1);
+						//카운터 락 해제
+						sem_release($countSemaphore);
 					}
 				}
 				break;
 			case LOCK_EX:
+				//카운터 락 획득
+				if(sem_acquire($countSemaphore)){
+					//카운터가 존재하지 않거나 0이라면
+					if(!apcu_exists($countKey) || ($count = apcu_fetch($countKey))==0){
+						apcu_store($countKey, -1);
+						//락 획득
+						sem_acquire($lockSemaphore);
+						//카운터 락 해제
+						sem_release($countSemaphore);
+					}
+					//카운터가 0이 아니라면 공유락 또는 배제락 상태이므로 락이 풀릴 때까지 대기한다.
+					else{
+						$acquired = sem_acquire($lockSemaphore, true);
+						//카운터 락 해제
+						sem_release($countSemaphore);
+						//락 획득할 때까지 대기한다.
+						if(!$acquired){sem_acquire($lockSemaphore);}
+						//락을 획득하면 다시 카운터 락을 걸고 카운터에 -1저장하고 카운터 락 해제한다.
+						if(sem_acquire($countSemaphore)){
+							apcu_store($countKey, -1);
+							//카운터 락 해제
+							sem_release($countSemaphore);
+						}
+					}
+				}
 				break;
 			case LOCK_UN:
+				//카운터 락 획득
+				if(sem_acquire($countSemaphore)){
+					
+				}
 				break;
 			case LOCK_SH|LOCK_NB:
 				break;
