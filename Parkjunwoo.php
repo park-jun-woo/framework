411de69d1d1480153d22b5f3b94ae7bce981afc9
mados <mados@DESKTOP-4BMQ5JQ>
2023-09-24 20:24:57 +0900

    .

--- Parkjunwoo.php
+++ Parkjunwoo.php
@@ -48,12 +48,12 @@ class Parkjunwoo{
 		}
 		//현재 접속한 앱
 		$this->thisApp = $this->code["app"][$this->code["domain-app"][$_SERVER["SERVER_NAME"]]];
+		//캐시형 데이터베이스
+		$this->model = new Zeolite($this);
 		//사용자 세션
 		$this->user = new User($this);
 		//요청 분석
 		$this->request = new Request($this);
-		//캐시형 데이터베이스
-		$this->model = new Zeolite($this);
 		//요청 실행
 		$this->controller = new Controller($this);
 	}
