b3767f1cdf0a41294e9283b9bd1a2cdc02823faf
park-jun-woo <mail@parkjunwoo.com>
2024-05-17 14:52:25 +0900

    .

--- src/Parkjunwoo/Core/Model.php
+++ src/Parkjunwoo/Core/Model.php
@@ -11,16 +11,16 @@ class Model{
     public function __construct(Parkjunwoo $man, string $entityName){
 
     }
-    public function get(array $entity, array $where):array{
+    public function get(array $where):array{
 
     }
-    public function post(array $entity, array $data):int{
+    public function post(array $data):int{
 
     }
-    public function put(array $entity, array $data, array $where):bool{
+    public function put(array $data, array $where):bool{
 
     }
-    public function delete(array $entity, array $where):bool{
+    public function delete(array $where):bool{
 
     }
 }
